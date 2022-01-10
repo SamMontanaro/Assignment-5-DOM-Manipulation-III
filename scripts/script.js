afce66317f358a06aa65511ed62b5f3dce45974f
Sam Montanaro <sammontanaro12@gmail.com>
2022-01-10 04:23:13 -0500

    Added comments for documentation of each user story

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,7 @@ window.onload = () => {
     let currentColSize = 3;
     color = document.getElementById("color").value;
 
+    // Check for mousedown/mouseup, to be used for click + hold functionality
     document.addEventListener("mousedown", () => {
         isDown = true;
     })
@@ -13,6 +14,7 @@ window.onload = () => {
         isDown = false;
     })
 
+    // Give starting cells click and click + hold functionality
     Array.from(document.querySelectorAll("td")).forEach(cell => {
         cell.addEventListener("click", () => {
             cell.className = color;
@@ -24,9 +26,11 @@ window.onload = () => {
         })
     });
 
+    // Add a row
     document.getElementById("rowAdd").addEventListener("click", () => {
         const newRow = document.createElement("tr");
         table.append(newRow);
+        // Use the current column size to know how many columns are in the new row
         for (let i = 0; i < currentColSize; i++) {
             let cell = document.createElement("td");
             cell.classList.add("white");
@@ -41,10 +45,13 @@ window.onload = () => {
             newRow.append(cell);
         }
     })
+
+    // Add a column
     document.getElementById("colAdd").addEventListener("click", () => {
         const rows = Array.from(document.querySelectorAll("tr"));
         if (rows) {
             rows.forEach(row => {
+                // Append a new table data tag to each row, after making them the default color with proper click and click + hold functionality
                 let cell = document.createElement("td");
                 cell.classList.add("white");
                 cell.addEventListener("click", () => {
@@ -60,14 +67,20 @@ window.onload = () => {
             currentColSize++;
         }
     })
+
+    // Remove a row
     document.getElementById("rowDel").addEventListener("click", () => {
         const rows = document.querySelectorAll("tr");
         if (rows.length > 0) {
+            // Remove the last row, rather than the first
             rows[rows.length - 1].remove();
         }
     })
+
+    // Remove a column
     document.getElementById("colDel").addEventListener("click", () => {
         let rows = Array.from(document.querySelectorAll("tr"));
+        // Removes one table data from the end of each table row, to remove a column
         if (rows) {
             rows.forEach(row => {
                 row.children[row.childElementCount - 1].remove();
@@ -77,10 +90,12 @@ window.onload = () => {
     })
 }
 
+// Changes currently picked color on selection update
 let changeColor = () => {
     color = document.getElementById("color").value;
 }
 
+// Fill all unfilled cells with the currently selected color
 let fillUnfilled = () => {
     let whiteCells = Array.from(document.getElementsByClassName("white"));
     whiteCells.forEach(cell => {
@@ -88,6 +103,7 @@ let fillUnfilled = () => {
     })
 }
 
+// Fill all cells with the currently selected color
 let fillAll = () => {
     let cells = Array.from(document.querySelectorAll("td"));
     cells.forEach(cell => {
@@ -95,6 +111,7 @@ let fillAll = () => {
     })
 }
 
+// Clear all cells back to being white (default)
 let clearAll = () => {
     let cells = Array.from(document.querySelectorAll("td"));
     cells.forEach(cell => {
